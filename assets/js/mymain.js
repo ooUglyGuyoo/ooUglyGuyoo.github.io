3d7d7d8266a3051d0eb6ab0a7c73550113af2b01
ooUGoo <43401594+ooUGoo@users.noreply.github.com>
2022-06-28 15:12:32 +0800

    irlms

---
+++ assets/js/mymain.js
@@ -0,0 +1,100 @@
+class myHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+    <header class="header text-center">
+        <h1 class="blog-name pt-lg-4 mb-0"><a href="index.html">LYC's Web</a></h1>
+
+        <nav class="navbar navbar-expand-lg navbar-dark">
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+
+            <div id="navigation" class="collapse navbar-collapse flex-column">
+                <div class="profile-section pt-3 pt-lg-0">
+                    <img class="profile-image mb-3 rounded-circle mx-auto" src="assets/images/profile.jpg" alt="image">
+
+                    <div class="bio mb-3">My name is LIANG, Yuchen, you can call me Eric. I am from Beijing and now is based in Hong Kong. I am now a university student at HKUST.<br><a href="about.html">Learn more about me</a></div>
+                    <!--//bio-->
+                    <!-- <ul class="social-list list-inline py-3 mx-auto">
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in fa-fw"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-github-alt fa-fw"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-stack-overflow fa-fw"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-codepen fa-fw"></i></a></li>
+                    </ul> -->
+                    <!--//social-list-->
+                    <hr>
+                </div>
+                <!--//profile-section-->
+
+                <ul class="navbar-nav flex-column text-left">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i> Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="ugschool.html"><i class="fas fa-bookmark fa-fw mr-2"></i>UG School Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="others.html"><i class="fas fa-bookmark fa-fw mr-2"></i>Other Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="aboutme.html"><i class="fas fa-user fa-fw mr-2"></i>About Me</a>
+                    </li>
+                </ul>
+
+                <!-- <div class="my-2 my-md-3">
+                    <a class="btn btn-primary" href="https://themes.3rdwavemedia.com/" target="_blank">Get in Touch</a>
+                </div> -->
+            </div>
+        </nav>
+    </header>
+        `
+    }
+}
+
+customElements.define('my-header', myHeader)
+
+
+class myHead extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <head>
+            
+        </head>
+        `
+    }
+}
+
+customElements.define('my-head', myHead)
+
+
+class myFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="footer text-center py-2 theme-bg-dark">
+            
+            <small class="copyright">© 2022 LYC, Designed with <i class="fas fa-heart" style="color: #fb866a;"></i> by <a href="http://themes.3rdwavemedia.com" target="_blank">Xiaoying Riley</a> for developers</small><br>
+            <small><a href="https://beian.miit.gov.cn/">京ICP备2020037052号-1</a></small><br>
+            <small><img src="http://www.lyceric.com/wp-content/uploads/2021/02/cafa210b-8190-6c40-baf5-db3ab57ef252.png" class="wp-image-67 style-992-image style-local-176-f31-image" alt="">
+                京公网安备 11010802033071 号
+            </small>
+            <!--/* This template is released under the Creative Commons Attribution 3.0 License. Please keep the attribution link below when using for your own project. Thank you for your support. :) If you'd like to use the template without the attribution, you can buy the commercial license via our website: themes.3rdwavemedia.com */-->
+            
+
+        </footer>
+        `
+    }
+}
+
+customElements.define('my-footer', myFooter)
+
+// class myHeader extends HTMLElement {
+//     connectedCallback(){
+//         this.innerHTML = `
+
+//         `
+//     }
+// }
+
+// customElements.define('my-header',myHeader)
